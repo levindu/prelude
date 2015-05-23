2cc2536ca2d94dfe231d82d4651de456a9f76648
Levin Du <zslevin@gmail.com>
2015-05-23 10:05:21 +0800

    New module prelude-multiple-cursors.el

---
+++ modules/prelude-multiple-cursors.el
@@ -0,0 +1,42 @@
+;;; prelude-multiple-cursors.el --- Emacs Prelude: multiple-cursors configuration.
+;;
+;; Copyright © 2015 Levin Du
+;;
+;; Author: Levin Du <zslevin@gmail.com>
+;; URL: http://batsov.com/prelude
+;; Version: 1.0.0
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Some basic configuration for multiple-cursors.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+(use-package
+  multiple-cursors
+  :bind (("C-S-c" . mc/edit-lines)
+         ("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)))
+
+(provide 'prelude-multiple-cursors)
+;;; prelude-multiple-cursors.el ends here
