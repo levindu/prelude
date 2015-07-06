b5eb74cad9fe5376d2a1cebc5b9c493717482261
Levin Du <zslevin@gmail.com>
2015-07-06 18:23:26 +0800

    prelude-org: turn on org-mode for *.org_archive file

--- modules/prelude-org.el
+++ modules/prelude-org.el
@@ -32,7 +32,7 @@
 
 ;;; Code:
 
-(add-to-list 'auto-mode-alist '("\\.org\\’" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\)\\'" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
